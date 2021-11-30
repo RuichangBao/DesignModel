07135ff5011d400263991882c10dcbe00c13c322
RuichangBao <1294838346@qq.com>
2021-11-30 21:04:03 +0800

    aaa

---
+++ DesignMode/Single.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignMode
+{
+    /// <summary>
+    /// 单例模式
+    /// </summary>
+    public class Single
+    {
+        private Single()
+        {
+
+        }
+        private static Single instance;
+
+        public static Single Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Single();
+                }
+                return instance;
+            }
+        }
+    }
+}
