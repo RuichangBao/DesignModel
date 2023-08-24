083af2cd442c0f29ce4f220c891602cc022b4307
RuichangBao <1294838346@qq.com>
2023-08-24 18:46:14 +0800

    设计模式

---
+++ DesignMode/Singleton/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace SingletonMode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            _ = Singleton.Instance;
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ DesignMode/Singleton/Singleton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//单例模式
+namespace SingletonMode
+{
+    public class Singleton
+    {
+        private static Singleton instance;
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Singleton();
+                return instance;
+            }
+        }
+        private Singleton()
+        {
+            Console.WriteLine("单例模式构造函数");
+        }
+    }
+}
